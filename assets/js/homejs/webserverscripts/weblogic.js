8ef5642465cf11a1674270f1bfad7731649b6aa9
basithahamed <basithhahamed@gmail.com>
2023-02-09 12:47:46 +0530

    Made ProAppBackup to ProApp

--- assets/js/homejs/webserverscripts/weblogic.js
+++ assets/js/homejs/webserverscripts/weblogic.js
@@ -2,7 +2,7 @@ let userObj = {
     userId : USERID,
     userName : USERNAME
 }
-let webConnection = new WebSocket("ws://localhost:8787/ProAppBackup/proapp?data=vicky");
+let webConnection = new WebSocket("ws://localhost:8787/ProApp/proapp?data=vicky");
 webConnection.onopen = function(event){
     console.log(event.log);
 }
